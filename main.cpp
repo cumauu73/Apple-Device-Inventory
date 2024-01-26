f40a38b3eb16df6055f0a50c2f171ea0cfb2861f
Cuma Ugur <cumaugur@Cumas-MBP.attlocal.net>
2024-01-25 19:14:31 -0600

    all model numbers written

--- main.cpp
+++ main.cpp
@@ -1,16 +1,88 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main() {
-  string SerialNumber;
-  cout << "Enter the Serial Number: ";
-  cin >> SerialNumber;
-  if (SerialNumber =="abc") {
-    cout << "The serial number is not valid.";
-  }
-  else {
-    cout << "hi correct";
-  }
+    string ModelNumber;
+    cout << "Please Enter the Model Number: ";
+    cin >> ModelNumber;
+
+    if (ModelNumber == "A2436" || ModelNumber == "A2437" || ModelNumber == "A2764" || ModelNumber == "A2766") {
+        cout << "iPad Pro 12.9-inch (6th generation)";
+    } else if (ModelNumber == "A2759" || ModelNumber == "A2761" || ModelNumber == "A2435" || ModelNumber == "A2762") {
+        cout << "iPad Pro 11-inch (4th generation)";
+    } else if (ModelNumber == "A2378" || ModelNumber == "A2461" || ModelNumber == "A2379" || ModelNumber == "A2462") {
+        cout << "iPad Pro 12.9-inch (5th generation)";
+    } else if (ModelNumber == "A2377" || ModelNumber == "A2459" || ModelNumber == "A2301" || ModelNumber == "A2460") {
+        cout << "iPad Pro 11-inch (3rd generation)";
+    } else if (ModelNumber == "A2229" || ModelNumber == "A2069" || ModelNumber == "A2232" || ModelNumber == "A2233") {
+        cout << "iPad Pro 12.9-inch (4th generation)";
+    } else if (ModelNumber == "A2228" || ModelNumber == "A2068" || ModelNumber == "A2230" || ModelNumber == "A2231") {
+        cout << "iPad Pro 11-inch (2nd generation)";
+    } else if (ModelNumber == "A1876" || ModelNumber == "A2014" || ModelNumber == "A1895" || ModelNumber == "A1983") {
+        cout << "iPad Pro 12.9-inch (3rd generation)";
+    } else if (ModelNumber == "A1980" || ModelNumber == "A2013" || ModelNumber == "A1934" || ModelNumber == "A1979") {
+        cout << "iPad Pro 11-inch";
+    } else if (ModelNumber == "A1670" || ModelNumber == "A1671" || ModelNumber == "A1821") {
+        cout << "iPad Pro 12.9-inch (2nd generation)";
+    } else if (ModelNumber == "A1701" || ModelNumber == "A1709" || ModelNumber == "A1852") {
+        cout << "iPad Pro (10.5-inch)";
+    } else if (ModelNumber == "A1673" || ModelNumber == "A1674" || ModelNumber == "A1675") {
+        cout << "iPad Pro (9.7-inch)";
+    } else if (ModelNumber == "A1584" || ModelNumber == "A1652") {
+        cout << "iPad Pro (12.9-inch)";
+    } else if (ModelNumber == "A2588" || ModelNumber == "A2589" || ModelNumber == "A2591") {
+        cout << "iPad Air (5th generation)";
+    } else if (ModelNumber == "A2316" || ModelNumber == "A2324" || ModelNumber == "A2325" || ModelNumber == "A2072") {
+        cout << "iPad Air (4th generation)";
+    } else if (ModelNumber == "A2152" || ModelNumber == "A2123" || ModelNumber == "A2153" || ModelNumber == "A2154") {
+        cout << "iPad Air (3rd generation)";
+    } else if (ModelNumber == "A1566" || ModelNumber == "A1567") {
+        cout << "iPad Air 2";
+    } else if (ModelNumber == "A1474" || ModelNumber == "A1475" || ModelNumber == "A1476") {
+        cout << "iPad Air";
+    } else if (ModelNumber == "A2567" || ModelNumber == "A2568" || ModelNumber == "A2569") {
+        cout << "iPad mini (6th generation)";
+    } else if (ModelNumber == "A2133" || ModelNumber == "A2124" || ModelNumber == "A2126" || ModelNumber == "A2125") {
+        cout << "iPad mini (5th generation)";
+    } else if (ModelNumber == "A1538" || ModelNumber == "A1550") {
+        cout << "iPad mini 4";
+    } else if (ModelNumber == "A1599" || ModelNumber == "A1600") {
+        cout << "iPad mini 3";
+    } else if (ModelNumber == "A1489" || ModelNumber == "A1490" || ModelNumber == "A1491") {
+        cout << "iPad mini 2";
+    } else if (ModelNumber == "A1432" || ModelNumber == "A1454" || ModelNumber == "A1455") {
+        cout << "iPad mini";
+    } else if (ModelNumber == "A2696" || ModelNumber == "A2757" || ModelNumber == "A2777" || ModelNumber == "A3162") {
+        cout << "iPad (10th generation)";
+    } else if (ModelNumber == "A2602" || ModelNumber == "A2604" || ModelNumber == "A2603" || ModelNumber == "A2605") {
+        cout << "iPad (9th generation)";
+    } else if (ModelNumber == "A2270" || ModelNumber == "A2428" || ModelNumber == "A2429" || ModelNumber == "A2430") {
+        cout << "iPad (8th generation)";
+    } else if (ModelNumber == "A2197" || ModelNumber == "A2200" || ModelNumber == "A2198") {
+        cout << "iPad (7th generation)";
+    } else if (ModelNumber == "A1893" || ModelNumber == "A1954") {
+        cout << "iPad (6th generation)";
+    } else if (ModelNumber == "A1822" || ModelNumber == "A1823") {
+        cout << "iPad (5th generation)";
+    } else if (ModelNumber == "A1458" || ModelNumber == "A1459" || ModelNumber == "A1460") {
+        cout << "iPad (4th generation)";
+    } else if (ModelNumber == "A1416" || ModelNumber == "A1430" || ModelNumber == "A1403") {
+        cout << "iPad (3rd generation)";
+    } else if (ModelNumber == "A1395" || ModelNumber == "A1396" || ModelNumber == "A1397") {
+        cout << "iPad 2";
+    } else if (ModelNumber == "A1219" || ModelNumber == "A1337
+
+  
+
+
+
+
+
+
+
+
+
    return 0;
-}
+} 
